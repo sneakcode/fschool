e5b0d375ba4d49ca32ef49ec06305d83ae698e56
gerney <btcsideveloper@gmail.com>
2024-03-25 16:47:15 +0800

    database settings and auth settings

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -22,9 +22,9 @@
 return array(
 	'default' => array(
 		'connection' => array(
-			'dsn'      => 'mysql:host=localhost;dbname=fuel_dev',
+			'dsn'      => 'mysql:host=localhost;dbname=fschool',
 			'username' => 'root',
-			'password' => 'root',
+			'password' => '',
 		),
 	),
 );
